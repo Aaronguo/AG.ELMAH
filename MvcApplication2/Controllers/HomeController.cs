2350f715a9babbd6cea08a40d5db05ad029ab3c1
小乙 <Aaronguo2580@hotmail.com>
2012-10-25 15:52:54 +0800

    错误日志处理模版使用及说明

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "欢迎使用 ASP.NET MVC!";
+
+            //int i = Convert.ToInt32("ty");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
